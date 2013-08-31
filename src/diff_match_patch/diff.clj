d066c49ba929279321f5f008b9076128d42c8f62
Timo Sulg <timgluz@gmail.com>
2013-08-31 18:59:32 +0200

    Fixed type specifiers; added to-hiccup formatter for diffs;

--- src/diff_match_patch/diff.clj
+++ src/diff_match_patch/diff.clj
@@ -1,5 +1,6 @@
 (ns diff-match-patch.core
-  (:import [name.fraser.neil.plaintext diff_match_patch$Operation]))
+  (:import [name.fraser.neil.plaintext diff_match_patch$Operation])
+  (:require [clojure.string :as string]))
 
 (defn diff [txt1 txt2] 
    "Finds the differences between 2 texts."
@@ -35,15 +36,15 @@
 (defn levenshtein [diffs]
   (.diff_levenshtein *diffor* diffs))
 
-(defn common-prefix [:^String txt1 :^String txt2]
+(defn common-prefix [^:String txt1 ^:String txt2]
   "Determines the common prefix of 2 strings."
   (.diff_commonPrefix *diffor* txt1 txt2))
 
-(defn common-suffix [:^String txt1 :^String txt2]
+(defn common-suffix [^:String txt1 ^:String txt2]
   "Determines the common suffix of two strings."
   (.diff_commonSuffix *diffor* txt1 txt2))
 
-(defn x-index [diffs, :^Integer loc]
+(defn x-index [diffs, ^:Integer loc]
   "Computes and returns the equivalent location in text2 for location is text1"
   (.diff_xIndex *diffor* diffs loc))
 
@@ -66,10 +67,31 @@
   "
   (.diff_toDelta *diffor* diffs))
 
-(defn from-delta [:^String text1 :^String delta-text]
+(defn from-delta [^:String text1 ^:String delta-text]
   "given the original text1 and an encoded string which describes the operations
   required to transform text1 into text2, and computes the full diff."
   (.diff_fromDelta *diffor* text1 delta-text))
 
+(defn- get-op [diff]
+  (let [op-string (.. diff operation toString)]
+    (-> op-string str string/lower-case keyword)))
 
+(defn- get-val [diff]
+  (.text diff))
+
+(defn to-hiccup [diffs]
+  "Converts a diff list into a pretty Hiccup presentation"
+  (let [pos (atom 0)]
+    (for [diff diffs]
+      (let [op-key (get-op diff)
+            diff-val (get-val diff)
+            diff-length (count diff-val)
+            current-pos @pos]
+        (when-not (= :delete op-key)
+          (swap! pos #(+ diff-length %1)))
+        (vector op-key 
+                {:class (name op-key)
+                 :data-length diff-length
+                 :data-start current-pos} 
+                diff-val)))))
 
